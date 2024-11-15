b5f8061ae5478c331f2d1843cd46fc735fdc8fd6
João Portella <joao@portella.dev>
2024-11-15 18:38:53 -0300

    Add spring boot main class

---
+++ src/main/java/dev/portella/crudwebapp/CrudWebApp.java
@@ -0,0 +1,11 @@
+package dev.portella.crudwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CrudWebApp.class, args);
+    }
+}
