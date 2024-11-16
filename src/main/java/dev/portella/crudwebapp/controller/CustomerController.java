2f2c349df3837704a72c648d0e9ecddf0083a84b
João Portella <joao@portella.dev>
2024-11-16 05:40:31 -0300

    Add Customer controller validation

--- src/main/java/dev/portella/crudwebapp/controller/CustomerController.java
+++ src/main/java/dev/portella/crudwebapp/controller/CustomerController.java
@@ -1,10 +1,17 @@
 package dev.portella.crudwebapp.controller;
 
-import dev.portella.crudwebapp.model.Customer;
-import dev.portella.crudwebapp.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import dev.portella.crudwebapp.model.Customer;
+import dev.portella.crudwebapp.service.CustomerService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
@@ -34,14 +41,18 @@ public class CustomerController {
     }
 
     @PostMapping
-    public String save(Customer customer) {
+    public String save(@Valid @ModelAttribute Customer customer, BindingResult result) {
+        if (result.hasErrors()) {
+            return FORM;
+        }
         customerService.save(customer);
         return REDIRECT;
     }
 
     @GetMapping("/edit/{id}")
     public String editForm(@PathVariable Long id, Model model) {
-        model.addAttribute(MODEL, customerService.findById(id).orElseThrow(() -> new IllegalArgumentException("ID inválido: " + id)));
+        model.addAttribute(MODEL,
+                customerService.findById(id).orElseThrow(() -> new IllegalArgumentException("ID inválido: " + id)));
         return FORM;
     }
 
