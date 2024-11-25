df89715411e81231f0b8e9c6b8573e87abb86099
João Portella <joao@portella.dev>
2024-11-25 02:41:26 -0300

    Update theme-toggle.js

--- src/main/resources/static/js/theme-toggle.js
+++ src/main/resources/static/js/theme-toggle.js
@@ -19,20 +19,27 @@ function setTheme(theme) {
   if (theme === "dark") {
     body.classList.remove("bg-light", "text-dark");
     body.classList.add("bg-dark", "text-light");
-    mainContent.classList.remove("bg-light", "text-dark");
-    mainContent.classList.add("bg-dark", "text-light");
+
+    if (mainContent) {
+      mainContent.classList.remove("bg-light", "text-dark");
+      mainContent.classList.add("bg-dark", "text-light");
+    }
+
     listGroupItems.forEach((item) => {
       item.classList.remove("bg-light", "text-dark");
       item.classList.add("bg-dark", "text-light");
     });
+
     links.forEach((link) => {
       link.classList.remove("text-dark");
       link.classList.add("text-light");
     });
+
     tables.forEach((table) => {
       table.classList.remove("table-light");
       table.classList.add("table-dark");
     });
+
     inputs.forEach((input) => {
       input.classList.remove("bg-light", "text-dark");
       input.classList.add("bg-dark", "text-light");
@@ -40,20 +47,27 @@ function setTheme(theme) {
   } else {
     body.classList.remove("bg-dark", "text-light");
     body.classList.add("bg-light", "text-dark");
-    mainContent.classList.remove("bg-dark", "text-light");
-    mainContent.classList.add("bg-light", "text-dark");
+
+    if (mainContent) {
+      mainContent.classList.remove("bg-dark", "text-light");
+      mainContent.classList.add("bg-light", "text-dark");
+    }
+
     listGroupItems.forEach((item) => {
       item.classList.remove("bg-dark", "text-light");
       item.classList.add("bg-light", "text-dark");
     });
+
     links.forEach((link) => {
       link.classList.remove("text-light");
       link.classList.add("text-dark");
     });
+
     tables.forEach((table) => {
       table.classList.remove("table-dark");
       table.classList.add("table-light");
     });
+
     inputs.forEach((input) => {
       input.classList.remove("bg-dark", "text-light");
       input.classList.add("bg-light", "text-dark");
