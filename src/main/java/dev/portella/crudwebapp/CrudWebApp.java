f2ab776829ab7542653050aed5450f1641d09e1d
João Portella <joao@portella.dev>
2024-11-27 18:26:59 -0300

    Add SpringBootServletInitializer

--- src/main/java/dev/portella/crudwebapp/CrudWebApp.java
+++ src/main/java/dev/portella/crudwebapp/CrudWebApp.java
@@ -2,9 +2,17 @@ package dev.portella.crudwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrudWebApp {
+public class CrudWebApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CrudWebApp.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CrudWebApp.class, args);
     }
